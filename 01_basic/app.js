d347210e1376c93eed8464552bb8dfaeff09e5cf
Sahil-tried-coding <sahiltriedcoding@gmail.com>
2024-08-28 04:02:57 -0400

    intial commit

---
+++ 01_basic/app.js
@@ -0,0 +1,6 @@
+import gsap from "gsap"
+let box_1 = document.getElementsByClassName("box-1")
+let box_2 = document.getElementsByClassName("box-2")
+let box_3 = document.getElementsByClassName("box-3")
+
+
